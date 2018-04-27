9c47a4a68978031a3ec8e462d257ffa6fd47f888
Your Name <you@example.com>
2018-04-27 17:28:15 -0500

    second commit

--- RandLetter/R/RandLetter.R
+++
@@ -1,7 +0,0 @@
-#' Randomly Choose a Number of Letters
-#' @param n number of letters
-#' @author Prayogi, Egi
-#' @return A number of letters
-#' @export
-
-RandLetter <- function(n = 1){sample(LETTERS, n, TRUE)}
